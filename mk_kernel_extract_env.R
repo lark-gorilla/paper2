3c15d973630ce1b13b94a5abb2a9b2af3cf58b2d
lark-gorilla <mark.gr.miller@gmail.com>
2016-09-23 16:38:39 +0100

    Swapped layout of script, psuedo absence generation first now. Added buffers to kill local colony points before kernel construction. Started to pull in env rasters

--- mk_kernel_extract_env.R
+++ mk_kernel_extract_env.R
@@ -13,39 +13,10 @@ library(rgeos)
 
 setwd("~/grive/phd/analyses/paper2")
 
-dat<-read.csv("~/grive/phd/analyses/tracking_data_pot/GPS_141516_clean_resamp_tripsplit_hmm_attribs.csv", h=T)
-
-# need to define h value (Scale), thinking max dist in data between 2 points (10 mins)
-max(dat$Vel)/6 #max velocity is 74 km/h, so is 12km in 10 mins (resmaple time)
-hscale<-12
-
-source("~/grive/phd/scripts/github_MIBA/batchUD.R")
-
-#source("~/grive/phd/scripts/MIBA_scripots_revised/BatchUD_revised.r")
-
-for(j in c(99,75,50,25))
-{
-  dat$ID<-j
-  UD_out<-batchUD(dat[with(dat, trip_type=="L" &
-                  Colony=='Heron' & Year=="2015"),],
-                  Scale = hscale, UDLev = j)
-  
-  if(j==99){all_UDs<-UD_out}else{all_UDs<-spRbind(all_UDs, UD_out)}
-  
-  plot(all_UDs, border=factor(all_UDs$id))
-}
-
-all_UDs <- spTransform(all_UDs, CRS=CRS("+proj=longlat +ellps=WGS84"))
-#I updated Lubuntu and now proj is handed correctly so can deal with WGS!! :)
-
-plot(all_UDs, border=factor(all_UDs$id), lwd=2)
-
-writeOGR(all_UDs, layer="trial", dsn="spatial", driver="ESRI Shapefile", verbose=TRUE, overwrite=T)
+######### ************** PSUEDO ABSENCE CONSTRUCTION ************** ##########
 
 ## to define area available to each colony constuct a radius (using max trip dist)
 ## then clip out land
-
-d1<-max(dat$ColDist) #1183621 m
 
 Colony_heron<-SpatialPoints(data.frame(Longitude=151.913773, Latitude=-23.44306 ), proj4string=CRS("+proj=longlat + datum=wgs84"))
 Colony_lhi<-SpatialPoints(data.frame(Longitude=159.05991, Latitude=-31.52459 ), proj4string=CRS("+proj=longlat + datum=wgs84"))
@@ -105,6 +76,89 @@ heron_psuedo<-spsample(heronBuffWgsclip, 2000, type="random")
 plot(heron_psuedo)
 lhi_psuedo<-spsample(lhiBuffWgsclip, 2000, type="random")
 
+######### ************** KERNEL CONSTRUCTION ************** ##########
+
+dat<-read.csv("~/grive/phd/analyses/tracking_data_pot/GPS_141516_clean_resamp_tripsplit_hmm_attribs.csv", h=T)
+heron_dat<-read.csv("~/grive/phd/sourced_data/Heron/heron_paper_Scarla/fi_data_04_sep/All LT PTT 2012 & 2013 day foraging data for kernels & 2006 2011.csv", h=T)
+
+# this bif of code cuts out a 300km buffer from the PTT data as per mcduie et al
+heron_old<-SpatialPointsDataFrame(SpatialPoints(data.frame(heron_dat$Longitude, heron_dat$Latitude),
+                                 proj4string=CRS( "+proj=longlat +ellps=WGS84")), data=heron_dat)
+
+heronBuffProj <- gBuffer(heronProj, width=300000, quadsegs=50) # 300km buffer as per mcduie et al
+heronBuffWgs <- spTransform(heronBuffProj, CRS=CRS( "+proj=longlat +ellps=WGS84"))
+plot(heron_old);plot(heronBuffWgs, add=T, border=2)
+ov1<-over(heron_old, heronBuffWgs) # Using over solves lots of problems for spatial subsetting http://gis.stackexchange.com/questions/63793/how-to-overlay-a-polygon-over-spatialpointsdataframe-and-preserving-the-spdf-dat
+heron_old<-heron_old[which(is.na((ov1))),]
+plot(heron_old)
+
+
+dat_old<-SpatialPointsDataFrame(SpatialPoints(data.frame(dat$Longitude, dat$Latitude),
+                                proj4string=CRS( "+proj=longlat +ellps=WGS84")), data=dat)
+
+datBuffProj <- gBuffer(lhiProj, width=20000, quadsegs=50) # first cut out 20 lhi buffer
+datBuffWgs <- spTransform(datBuffProj, CRS=CRS( "+proj=longlat +ellps=WGS84"))
+plot(dat_old);plot(datBuffWgs, add=T, border=2)
+ov1<-over(dat_old, datBuffWgs) # Using over solves lots of problems for spatial subsetting http://gis.stackexchange.com/questions/63793/how-to-overlay-a-polygon-over-spatialpointsdataframe-and-preserving-the-spdf-dat
+dat_old<-dat_old[which(is.na((ov1))),]
+plot(dat_old)
+
+datBuffProj <- gBuffer(heronProj, width=20000, quadsegs=50) # now cut out 20km heron buffer
+datBuffWgs <- spTransform(datBuffProj, CRS=CRS( "+proj=longlat +ellps=WGS84"))
+plot(dat_old);plot(datBuffWgs, add=T, border=2)
+ov1<-over(dat_old, datBuffWgs) # Using over solves lots of problems for spatial subsetting http://gis.stackexchange.com/questions/63793/how-to-overlay-a-polygon-over-spatialpointsdataframe-and-preserving-the-spdf-dat
+dat_old<-dat_old[which(is.na((ov1))),]
+plot(dat_old) # FINALLY!!
+
+
+
+d1<-max(dat$ColDist) #1183621 m
+# need to define h value (Scale), thinking max dist in data between 2 points (10 mins)
+max(dat$Vel)/6 #max velocity is 74 km/h, so is 12km in 10 mins (resmaple time)
+hscale<-20 # following McDuie et al 2015
+
+source("~/grive/phd/scripts/github_MIBA/batchUD.R")
+
+# decided to use 2011, 2013 PTT data, 2006 and 2012 seems to small 
+#heron_old[heron_old$Year==2011,]
+#dat[with(dat, trip_type=="L" & Colony=='Heron' & Year=="2015"),]
+
+for(j in c(99,75,50,25))
+{
+  heron_old$ID<-j
+  UD_out<-batchUD(heron_old[heron_old$Year==2013,],
+                  Scale = hscale, UDLev = j)
+  
+  if(j==99){all_UDs<-UD_out}else{all_UDs<-spRbind(all_UDs, UD_out)}
+  
+  plot(all_UDs, border=factor(all_UDs$id))
+}
+
+all_UDs <- spTransform(all_UDs, CRS=CRS("+proj=longlat +ellps=WGS84"))
+#I updated Lubuntu and now proj is handed correctly so can deal with WGS!! :)
+
+plot(all_UDs, border=factor(all_UDs$id), lwd=2)
+
+writeOGR(all_UDs, layer="LTHeronPTT2013", dsn="spatial", driver="ESRI Shapefile", verbose=TRUE, overwrite=T)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #read in climatology env data
 
 sst<-raster("~/grive/phd/sourced_data/env_data/climatologies/CPCsstn2016-03-01.nc")
@@ -115,6 +169,26 @@ wnd<-raster("~/grive/phd/sourced_data/env_data/climatologies/TNCwwww1995-03-31.n
 tmc<-raster("~/grive/phd/sourced_data/env_data/climatologies/thermocline_3.tif")
 smt<-raster("/home/mark/grive/phd/sourced_data/env_data/seamounts/d_seamounts_wgs.tif")
 bty<-raster("~/grive/phd/sourced_data/env_data/phd_bathy/GRIDONE_2D_100.0_-45.0_180.0_40.0.nc")
+
+#read in seapodym data
+
+bet_adu<-raster("/home/mark/grive/phd/sourced_data/SEAPODYM/month_ave_intermin_1deg_ref2015/bet_adu_03_ave.tif")
+bet_juv<-raster("/home/mark/grive/phd/sourced_data/SEAPODYM/month_ave_intermin_1deg_ref2015/bet_juv_03_ave.tif")
+bet_tot<-raster("/home/mark/grive/phd/sourced_data/SEAPODYM/month_ave_intermin_1deg_ref2015/bet_tot_03_ave.tif")
+skj_adu<-raster("/home/mark/grive/phd/sourced_data/SEAPODYM/month_ave_intermin_1deg_ref2015/skj_adu_03_ave.tif")
+skj_juv<-raster("/home/mark/grive/phd/sourced_data/SEAPODYM/month_ave_intermin_1deg_ref2015/skj_juv_03_ave.tif")
+skj_tot<-raster("/home/mark/grive/phd/sourced_data/SEAPODYM/month_ave_intermin_1deg_ref2015/skj_tot_03_ave.tif")
+yft_adu<-raster("/home/mark/grive/phd/sourced_data/SEAPODYM/month_ave_intermin_1deg_ref2015/yft_adu_03_ave.tif")
+yft_juv<-raster("/home/mark/grive/phd/sourced_data/SEAPODYM/month_ave_intermin_1deg_ref2015/yft_juv_03_ave.tif")
+yft_tot<-raster("/home/mark/grive/phd/sourced_data/SEAPODYM/month_ave_intermin_1deg_ref2015/yft_tot_03_ave.tif")
+
+
+
+
+
+
+
+
 
 
 
