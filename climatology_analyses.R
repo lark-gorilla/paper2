fa181a5d885e80ef139af4e3448047813eca4b8a
lark-gorilla <mark.gr.miller@gmail.com>
2016-09-25 17:52:48 +0100

    Start of climatology analyses

---
+++ climatology_analyses.R
@@ -0,0 +1,21 @@
+# 25/09/16 Lancaster, UK
+# Model climatology data comparing foraging areas against background 
+
+rm(list=ls())
+library(ggplot2)
+library(reshape2)
+
+setwd("~/grive/phd/analyses/paper2")
+
+dat<-read.csv("spreads/paper2_extractionV2.csv", h=T)
+
+d1<-melt(dat, id.vars=c("Latitude", "Longitude", "dset", "dtyp"))
+
+p<-ggplot(d1, aes(dset, value))
+p+geom_boxplot()+facet_wrap(~variable, scale="free")
+
+## We're going to use the Information Theoretic approach to test presence (50%UD) absence (max range)
+## of WTSH using a ?glm? testing oceanographic vs tuna variables using AIC. We will do this 
+## seperately for each colony.
+
+# first we need to remove outliers and transform some variables
