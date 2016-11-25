acf4424b1268e1aa58137d2eaa464dd1b198e197
lark-gorilla <mark.gr.miller@gmail.com>
2016-11-25 14:06:26 +0000

    Final run. Specified models, selected datasets and chosen variables and methods.

--- climatology_analyses_final.R
+++ climatology_analyses_final.R
@@ -64,23 +64,25 @@ qplot(Longitude, Latitude, data=dat)+geom_point(data=dat[dat$chl<1500,], aes(Lon
 
 dat[which(dat$chl>1500),]$chl<-NA
 # also knock out some dodgy looking wind values
-dat[is.na(dat$chl),]$wnd<-NA
+#dat[is.na(dat$chl),]$wnd<-NA
 # and some weird sst
 dat[which(dat$sst<0),]$sst<-NA
+#really slow winds near png<-NA
+#dat[which(dat$wnd<6),]$wnd<-NA
 #weird ekem point
-dat[which(dat$ekm< -0.00001),]$ekm<-NA
+#dat[which(dat$ekm< -0.00001),]$ekm<-NA
 #remove skj_juv outliers
-dat[which(dat$skj_juv_03_ave>28),]$skj_juv_03_ave<-NA
+#dat[which(dat$skj_juv_03_ave>28),]$skj_juv_03_ave<-NA
 
 dat$PA<-0
 dat[dat$Count>0,]$PA<-1 # making presence absence variable
 
 # remove some outlier presence clusters that cause modeeling issues later
 dat<-dat[-which(dat$dset=="Heron"& dat$PA==1 & dat$Longitude<153.04 &
-                  dat$Latitude< -21.44),]
+                 dat$Latitude< -21.44),]
 
 dat<-dat[-which(dat$dset=="Heron"& dat$PA==1 & dat$Longitude<152.5 &
-                  dat$Latitude< -18.74),]
+                 dat$Latitude< -18.74),]
 
 dat<-dat[-which(dat$dset=="Heron" & dat$PA==1 & dat$Longitude<151.4),]
 
@@ -109,6 +111,8 @@ dat$chl_log<-log(dat$chl)
 dat$smt_sqt<-sqrt(dat$smt)
 # changes BET from g/m2 to kg/km2 (bigger units in model)
 dat$bet_adu_03_ave<-(dat$bet_adu_03_ave*1000000)/1000 
+
+qplot(data=dat[dat$dset=="Heron",], x=wnd, bins=50)+facet_grid(PA~.)
 
 # how we looking now
 d1<-melt(dat, id.vars=c("Latitude", "Longitude", "dset", "Count", "PA"))
@@ -143,12 +147,6 @@ dat_heron<-rbind(heronP, heronPA)
 #dat_heron<-sp2@data
 #nrow(dat_heron) 
 
-lhiP<-dat[dat$dset=="LHI" & dat$Count>0,]
-lhiA<-dat[dat$dset=="LHI" & dat$Count==0,]
-lhiPA<-lhiA[sample(1:nrow(lhiA), 3200),]
-dat_lhi<-rbind(lhiP, lhiPA)
-
-
 # finding distance to nearest point after na.omit, this will identify isolated points
 # proj dat into m units for gDistance
 sp1<-SpatialPointsDataFrame(dat_heron[,1:2], data=dat_heron, proj4string=CRS("+proj=longlat + datum=wgs84"))
@@ -163,13 +161,8 @@ hist(dat_heron$min.d)
 plot(Latitude~Longitude, dat_heron)
 points(Latitude~Longitude, dat_heron[dat_heron$min.d>75000,], col=2)
 
-dat_heron<-dat_heron[-which(dat_heron$min.d>75000),] #remove poor neighbors (very few)!
+dat_heron<-dat_heron[dat_heron$min.d<75000,]
 
-# z-transform (scale and center) to make variables comparable on same scale
-# not doing that now!
-#library(vegan)
-#dat_heron<-cbind(dat_heron[,c(1,2,24:25)],
-#                 decostand(dat_heron[,c(5:7, 9,10,12:23)], method="standardize"))
 
 
 # now have a look at collinearity
@@ -233,6 +226,29 @@ g
 # and YFT numbers (Coral Sea) at one end and High Chlorophyll, SHD 
 # and deep thermocline at the other end (Tasman sea)
 
+summary(enviro_rda)$species
+
+glm_pca<-data.frame(PA=dat_heron$PA, enviro.sites.scores)
+##names(glm_pca)[names(glm_pca)=="PC1"]<-"inv_latitude"
+
+#try PC regression
+m2<-glm(PA~PC1+PC2+PC3+PC4, data=glm_pca, family="binomial")
+
+resglm<-residuals(m2, type="pearson")
+summary(m2)
+print(sum((resid(m2, type="pearson")^2))/df.residual(m2))
+print(roc.area(dat_heron$PA, fitted(m2))$A)
+
+
+# check final correltaions and VIFs
+pairs(dat_heron[,c(5,9,12,13 ,23,24) ], upper.panel = panel.smooth,lower.panel=panel.cor)
+
+pairs(dat_heron[,c(9,10,12,13 ,23,24) ], upper.panel = panel.smooth,lower.panel=panel.cor)
+# 
+
+vif(lm(1:nrow(dat_heron)~bet_adu_03_ave+
+        bty+smt_sqt+wnd+sst+chl_log,
+       data=dat_heron))
 
 # Now we see which variables are better suited to a second deg polynomial
 
@@ -244,12 +260,12 @@ g1+geom_jitter(height=0.1, size=0.5)+geom_smooth(method="glm", colour=2)+
 
 # We would do below for each variable individually to see the suitability of a poly
 
-m_lin<-glm(PA~bty,data=dat_heron, family="binomial")
-m_pol<-glm(PA~poly(bty, 2),data=dat_heron, family="binomial")
-d1<-data.frame(PA=dat_heron$PA, bty=dat_heron$bty, m_lin=fitted(m_lin), m_pol=fitted(m_pol))
-g1<-ggplot(data=d1, aes(y=PA, x=bty))
-g1+geom_jitter(height=0.1, size=0.5)+geom_line(aes(y=m_lin, x=bty), colour=2)+
-  geom_line(aes(y=m_pol, x=bty), colour=3)
+m_lin<-glm(PA~sst,data=dat_heron, family="binomial")
+m_pol<-glm(PA~poly(sst, 2),data=dat_heron, family="binomial")
+d1<-data.frame(PA=dat_heron$PA, sst=dat_heron$sst, m_lin=fitted(m_lin), m_pol=fitted(m_pol))
+g1<-ggplot(data=d1, aes(y=PA, x=sst))
+g1+geom_jitter(height=0.1, size=0.5)+geom_line(aes(y=m_lin, x=sst), colour=2)+
+  geom_line(aes(y=m_pol, x=sst), colour=3)
 anova(m_lin, m_pol)
 #poly model better in the case of skj_juv it gives the 0 to 1 warning so only use linear
 # It was important to work out what was causing the warning: glm.fit: fitted probabilities numerically 0 or 1 occurred
@@ -261,20 +277,33 @@ anova(m_lin, m_pol)
 # link spatially correlated variables, this term (RAC) is then included in
 # the final model to soak up the residual deviance 
 
-m2<-glm(PA~chl_log+wnd+
-          smt_sqt+poly(bty,2)+
-          bet_adu_03_ave+
-          sst,data=dat_heron, 
-        family="binomial");resglm<-residuals(m2, type="pearson")
-summary(m2)
-print(sum((resid(m2, type="pearson")^2))/df.residual(m2))
-print(roc.area(dat_heron$PA, fitted(m2))$A)
+# Choose either sst, chla, skj_adu as tropical tuna metric
+
+pairs(dat_heron[,c(5,9,12,13,16 ,23,24) ], upper.panel = panel.smooth,lower.panel=panel.cor)
+
+pairs(dat_heron[,c(9,12,13,16 ,24) ], upper.panel = panel.smooth,lower.panel=panel.cor)
+pairs(dat_heron[,c(5,9,12,13 ,24) ], upper.panel = panel.smooth,lower.panel=panel.cor)
+pairs(dat_heron[,c(9,12,13, 23,24) ], upper.panel = panel.smooth,lower.panel=panel.cor)
+
+m2a<-glm(PA~wnd+chl_log+smt_sqt+bty+bet_adu_03_ave,
+  data=dat_heron, family="binomial")
+m2b<-glm(PA~wnd+sst+smt_sqt+bty+bet_adu_03_ave,
+  data=dat_heron, family="binomial")
+m2c<-glm(PA~wnd+skj_adu_03_ave+smt_sqt+bty+bet_adu_03_ave,
+         data=dat_heron, family="binomial")
+
+anova(m2a, m2b, m2c);AIC(m2a, m2b, m2c)
+pR2(m2a)[4];pR2(m2b)[4];pR2(m2c)[4]
+
+m2<-glm(PA~tmc+wnd+chl_log+smt_sqt+bty+bet_adu_03_ave,
+         data=dat_heron, family="binomial");resglm<-residuals(m2, type="pearson")
 
 resglm<-residuals(m2, type="pearson")
 sp2<-SpatialPointsDataFrame(dat_heron[,1:2], data=data.frame(resglm), 
     proj4string=CRS("+proj=longlat + datum=wgs84"))
 bubble(sp2, zcol='resglm')
 #bubble(sp2, zcol='resglm', do.sqrt=F)
+
 
 #write.csv(cbind(dat_heron, resglm), "test5.csv", quote=FALSE)
 
@@ -291,10 +320,10 @@ RAC<-autocov_dist(resglm, cbind(dat_heron[,1], dat_heron[,2]),
 
 dat_heron$RAC<-RAC
 
-m3<-glm(PA~chl_log+wnd+
-          smt_sqt+poly(bty,2)+
+m3<-glm(PA~tmc+wnd+chl_log+
+          smt_sqt+bty+
           bet_adu_03_ave+
-          +sst+RAC,
+          RAC,
         data=dat_heron, family="binomial")
 
 plot(m3)
@@ -304,7 +333,26 @@ print(roc.area(dat_heron$PA, fitted(m3))$A)
 pR2(m3)
 summary(m3)
 
-# Save dataset we're happy with.
+corm2 <- correlog(dat_heron$Longitude, dat_heron$Latitude, residuals(m2, type="pearson"), na.rm=T,
+                 latlon=T, increment=25,resamp=1)
+corm3 <- correlog(dat_heron$Longitude, dat_heron$Latitude, residuals(m2, type="pearson"), na.rm=T,
+                  latlon=T, increment=25,resamp=1)
+
+# get odds
+
+exp(cbind(OR = coef(m3), confint(m3)))
+
+#Dominance analysis
+
+dm<-dm2(m3) # works!
+#I'll be using McFaddens' r2
+dm$contribution.average$r2.m
+
+#make sure it lines up
+sum(dm$contribution.average$r2.m)
+pR2(m3) # yep :)
+
+#Save dataset we're happy with.
 #write.csv(dat_heron, "spreads/dat_heron_3000PA_final.csv", row.names=F, quote=F)
 
 # looking at variable importance/contribution
@@ -341,21 +389,199 @@ summary(m3)
 # seemingly ok, but this is a very crude metric of importance, just scaled
 # coefficient and standard error (z value). Collinearity is messing this up.
 
-#Dominance analysis
 
-dm<-dm2(m3) # works!
-#I'll be using McFaddens' r2
-dm$contribution.average$r2.m
+##### LORD HOWE #####
 
-#make sure it lines up
-sum(dm$contribution.average$r2.m)
-pR2(m3) # yep :)
+lhiP<-dat[dat$dset=="LHI" & dat$Count>0,]
+lhiA<-dat[dat$dset=="LHI" & dat$Count==0,]
+lhiPA<-lhiA[sample(1:nrow(lhiA), 3200),]
+dat_lhi<-rbind(lhiP, lhiPA)
 
-# see if corr. sst and chla absorb each
-# others explanatory information
-dm2(update(m3,~.-  sst))$contribution.average$r2.m 
-dm2(update(m3,~.-  chl_log))$contribution.average$r2.m
-# yep pretty much.
+
+# finding distance to nearest point after na.omit, this will identify isolated points
+# proj dat into m units for gDistance
+sp1<-SpatialPointsDataFrame(dat_lhi[,1:2], data=dat_lhi, proj4string=CRS("+proj=longlat + datum=wgs84"))
+sp1proj <- spTransform(sp1, CRS=CRS("+proj=laea +lon_0=155 +lat_0=-21"))
+
+d <- gDistance(sp1proj, byid=T)
+
+min.d <- apply(d, 1, function(x) min(x[x>1000])) # pulls nearest point, which isnt itself or within 1000m projection error range
+dat_lhi$min.d<-min.d
+hist(dat_lhi$min.d)
+
+plot(Latitude~Longitude, dat_lhi)
+points(Latitude~Longitude, dat_lhi[dat_lhi$min.d>75000,], col=2)
+
+dat_lhi<-dat_lhi[dat_lhi$min.d<75000,] # remove orphaned points
+# now have a look at collinearity
+
+#pairs(dat_lhi[,-(1:2)], upper.panel = panel.smooth,lower.panel=panel.cor)
+pairs(dat_lhi[,5:23], upper.panel = panel.smooth,lower.panel=panel.cor)
+
+# final one for model
+pairs(dat_lhi[,c(5,6,9,10,12,13,14,16,17, 19,20, 23, 24) ], upper.panel = panel.smooth,lower.panel=panel.cor)
+# looking at it in full, i would be tempted to remove shd, tmc and yellowfin adult
+
+# Some serious collinearity need to investigate using PCA
+#then z transform to standardize
+enviro_std<-decostand(dat_lhi[,c(5,6,9,10,12,13,14,16,17, 19,20, 23, 24)], method="standardize")
+# takes all varibs but eke also juv and adu tnua forms
+
+# then do pca (just a scaled RDA)
+enviro_rda<-rda(enviro_std, scale=T)
+summary(enviro_rda, display=NULL)
+screeplot(enviro_rda) # badly scaled
+#full summary
+summary(enviro_rda)
+
+enviro.sites.scores<-as.data.frame(scores(enviro_rda, choices=1:4, display='sites', scaling=1)) 
+# i've put scaling to 1 for the sites to fit better on the plot
+
+# Now make some plots
+enviro.species.scores<-as.data.frame(scores(enviro_rda, display='species'))
+enviro.species.scores$Predictors<-colnames(enviro_std)
+#enviro.species.scores$Pred_codes<-codez
+head(enviro.species.scores)
+
+g<- ggplot()+
+  geom_segment(data=NULL, aes(y=-Inf, x=0, yend=Inf, xend=0), linetype='dotted')+
+  geom_segment(data=NULL, aes(y=0, x=-Inf, yend=0, xend=Inf), linetype='dotted')+
+  #geom_point(data=enviroPCA, aes(y=PC2, x=PC1, shape=treatshape, fill=treatfill),size=3)+scale_shape_identity()+scale_fill_identity()+
+  geom_segment(data=enviro.species.scores, aes(y=0, x=0, yend=PC2, xend=PC1), arrow=arrow(length=unit(0.3,'lines')), colour='red')+theme_classic() 
+g<-g+geom_text_repel(data=enviro.species.scores, aes(y=PC2, x=PC1, label=Predictors), segment.size=0, colour='red')
+
+eig<-eigenvals(enviro_rda)
+g<- g+scale_y_continuous(paste(names(eig[2]), sprintf('(%0.1f%% explained var.)', 100* eig[2]/sum(eig))))+
+  scale_x_continuous(paste(names(eig[1]), sprintf('(%0.1f%% explained var.)', 100* eig[1]/sum(eig))))
+
+g
+
+# Basically for Heron we have ~50% of the variation explained by PC1 
+# Which is effectively a gradient of Latitude with increasing SST and SKJ
+# and YFT numbers (Coral Sea) at one end and High Chlorophyll, SHD 
+# and deep thermocline at the other end (Tasman sea)
+
+summary(enviro_rda)$species
+
+
+# check final correltaions and VIFs
+pairs(dat_lhi[,c(9,12,13,14,16,17,19 ,23,24) ], upper.panel = panel.smooth,lower.panel=panel.cor)
+
+pairs(dat_lhi[,c(9,12,13 ,23,24) ], upper.panel = panel.smooth,lower.panel=panel.cor)
+# 
+pairs(dat_lhi[,c(5,9,12,19,24) ], upper.panel = panel.smooth,lower.panel=panel.cor)
+# 
+
+# 
+vif(lm(1:nrow(dat_lhi)~bet_adu_03_ave+
+         bty+smt_sqt+wnd+chl_log,
+       data=dat_lhi))
+
+# Now we see which variables are better suited to a second deg polynomial
+
+d1<-melt(dat_lhi, id.vars=c("PA", "Count", "dset"))
+# Very raw view of how a poly might better suit data than linear
+g1<-ggplot(data=d1, aes(y=PA, x=value))
+g1+geom_jitter(height=0.1, size=0.5)+geom_smooth(method="glm", colour=2)+
+  geom_smooth(method="glm", formula=y~poly(x,2), colour=3)+facet_wrap(~variable, scale="free")
+
+# We would do bl_cw for each variable individually to see the suitability of a poly
+
+m_lin<-glm(PA~sst,data=dat_lhi, family="binomial")
+m_pol<-glm(PA~poly(sst, 2),data=dat_lhi, family="binomial")
+d1<-data.frame(PA=dat_lhi$PA, sst=dat_lhi$sst, m_lin=fitted(m_lin), m_pol=fitted(m_pol))
+g1<-ggplot(data=d1, aes(y=PA, x=sst))
+g1+geom_jitter(height=0.1, size=0.5)+geom_line(aes(y=m_lin, x=sst), colour=2)+
+  geom_line(aes(y=m_pol, x=sst), colour=3)
+anova(m_lin, m_pol)
+#poly model better in the case of skj_juv it gives the 0 to 1 warning so only use linear
+# It was important to work out what was causing the warning: glm.fit: fitted probabilities numerically 0 or 1 occurred
+# it was the  skj_juv poly so its now removed and we can proceed!
+# not using ekm as its crap anyway
+
+# To account for spatial autocorrelation we calculate autocovariate terms
+# using the residuls of the fitted model and a neighbourhood distance to 
+# link spatially correlated variables, this term (RAC) is then included in
+# the final model to soak up the residual deviance 
+
+# Choose either sst, chla, skj_adu as tropical tuna metric
+
+pairs(dat_lhi[,c(5,9,12,13,16 ,23,24) ], upper.panel = panel.smooth,lower.panel=panel.cor)
+
+pairs(dat_lhi[,c(5,9,10,12,13,21 ,24) ], upper.panel = panel.smooth,lower.panel=panel.cor)
+pairs(dat_lhi[,c(5,9,10,12,13 ,24) ], upper.panel = panel.smooth,lower.panel=panel.cor)
+pairs(dat_lhi[,c(9,10,12,13, 23,24) ], upper.panel = panel.smooth,lower.panel=panel.cor)
+
+# chl_log not important
+# TMC better predictor than wind
+
+m2a<-glm(PA~wnd+tmc+chl_log+smt_sqt+bty+bet_adu_03_ave,
+         data=dat_lhi, family="binomial")
+# cant have bty and yft together
+m2b<-glm(PA~tmc+wnd+smt_sqt+yft_adu_03_ave+bet_adu_03_ave,
+         data=dat_lhi, family="binomial")
+
+anova(m2a, m2b);AIC(m2a, m2b)
+pR2(m2a)[4];pR2(m2b)[4]
+
+m2<-glm(PA~wnd+tmc+chl_log+smt_sqt+
+          bty+bet_adu_03_ave,
+         data=dat_lhi, family="binomial");resglm<-residuals(m2, type="pearson")
+
+summary(m2)
+
+print(sum((resid(m2, type="pearson")^2))/df.residual(m2))
+print(roc.area(dat_lhi$PA, fitted(m2))$A)
+
+resglm<-residuals(m2, type="pearson")
+sp2<-SpatialPointsDataFrame(dat_lhi[,1:2], data=data.frame(resglm), 
+                            proj4string=CRS("+proj=longlat + datum=wgs84"))
+bubble(sp2, zcol='resglm')
+#bubble(sp2, zcol='resglm', do.sqrt=F)
+
+#write.csv(cbind(dat_lhi, resglm), "test5.csv", quote=FALSE)
+
+
+#corglm <- correlog(dat_lhi$Longitude, dat_lhi$Latitude, residuals(m2, type="pearson"), na.rm=T,
+#                   latlon=T, increment=25,resamp=1)
+RAC<-autocov_dist(resglm, cbind(dat_lhi[,1], dat_lhi[,2]),
+                  nbs = 75, type = "one", zero.policy = T,
+                  style = "B", longlat=TRUE)
+# Model outputs are sensitive to neighborhood distance.
+# here we use 75 kms as happy medium between different
+# variables' resolutions. Remember points are at ~ 10km
+# most variables are at ~25km or 100 resampled at 25.
+
+dat_lhi$RAC<-RAC
+
+m3<-glm(PA~wnd+tmc+chl_log+
+          smt_sqt+bty+
+          bet_adu_03_ave
+          +RAC,
+        data=dat_lhi, family="binomial")
+
+plot(m3)
+print(sum((resid(m3, type="pearson")^2))/df.residual(m3))
+resglm<-residuals(m3, type="pearson")
+print(roc.area(dat_lhi$PA, fitted(m3))$A)
+pR2(m3)
+summary(m3)
+
+corm2 <- correlog(dat_heron$Longitude, dat_heron$Latitude, residuals(m2, type="pearson"), na.rm=T,
+                  latlon=T, increment=25,resamp=1)
+corm3 <- correlog(dat_heron$Longitude, dat_heron$Latitude, residuals(m2, type="pearson"), na.rm=T,
+                  latlon=T, increment=25,resamp=1)
+
+# get odds
+
+exp(cbind(OR = coef(m3), confint(m3)))
+
+# run dominanace analysis
+
+m3d<-dm2(m3)
+
+
+#! Understanding outputs !#
 
 # make same final model here but with non orthanonol polynomial,
 # this means that the coefficients are scaled correctly and more interpretable.
@@ -369,13 +595,47 @@ anova(m3, m3_interp)
 
 summary(m3_interp)
 
-exp(cbind(OR = coef(m3_interp), confint(m3_interp)))
+
 
 -53.26117519/(-28.50752856*2)
 # 0.9341598
 -1.922579e-03/(-2.427522e-07*2)
 # 3959.962
 
+-226.254389/(-86.476828*2)
+
+# Working on my understanding of interpreting odds ratio
+
+wnd<-c(4,4.2,4.3,4.2,4.8,4.3,5,5.3,5.4,5.6,5.8,5.9,6, 6.1,6.4)
+PA<-c(0,0,0,0,1,0,0,1,1,0,1,1,1,1,1)
+
+testdat<-data.frame(PA=PA,wnd=wnd)
+summary(glm.mod <- glm(PA~ wnd, family = "binomial", data=testdat))
+
+## logit = -0.8690 + (-1.0769) * x
+v1 <- -16.611  + (3.268)*5 # log odds of Presence at wind=5
+v2 <- -16.611  + (3.268)*6 # log odds of Presence at wind=6
+
+v1
+# -0.271
+v2
+# 2.997
+v2-v1
+# 3.268 ok so 1 unit change in wind increases log odds of
+# Presence by 3.268 - exactly what model summary says!
+# 
+
+# odds of v1 and v2 are
+exp(v1)
+# 0.7626165
+exp(v2)
+# 20.02537
+
+exp(v1)/(1+exp(v1))
+# 0.4326616 Probability of presence at wnd=5
+exp(v2)/(1+exp(v2))
+# 0.9524384 Probability of presence at wnd=6
+         
 exp(coef(m3))
 plogis(coef(m3))
 
@@ -385,21 +645,21 @@ plogis(coef(m3))
 # Where the RAC term is not included and therefore the responses of meaningful variables
 # are clearer.
 
-names(model.frame(m3))
-for (i in c("sst","chl_log","wnd","tmc","smt_sqt",                
+names(model.frame(m3)) # running on m2!
+for (i in c("chl_log", "sst","wnd","tmc","smt_sqt",               
             "bty","bet_adu_03_ave","RAC"))
 {
-  temp<-dat_heron[1,-4]
-  temp[1,]<-as.vector(apply(dat_heron[,-4], 2,median))
+  temp<-dat_lhi[1,-4]
+  temp[1,]<-as.vector(apply(dat_lhi[,-4], 2,median))
   
-  heron_pred<-data.frame(varib=dat_heron[,which(names(dat_heron)==i)], 
+  heron_pred<-data.frame(varib=dat_lhi[,which(names(dat_lhi)==i)], 
                          temp)
   names(heron_pred)[names(heron_pred)==i]<-"nope"
   names(heron_pred)[names(heron_pred)=="varib"]<-i
   
-  p1<-predict(m3, newdata=heron_pred, type="response")
+  p1<-predict(m2, newdata=heron_pred, type="response")
   
-  d1<-data.frame(PA=dat_heron$PA, varib=heron_pred[,1], pred=p1)
+  d1<-data.frame(PA=dat_lhi$PA, varib=heron_pred[,1], pred=p1)
   g1<-ggplot(data=d1, aes(y=PA, x=varib))
   print(i)
   print(g1+geom_jitter(height=0.1, size=0.5)+
